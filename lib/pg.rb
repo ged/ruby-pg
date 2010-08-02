8949df6f67258e608d65be919bb244da12c385c9
Michael Granger <ged@FaerieMUD.org>
2010-08-02 09:21:20 -0700

    Install extension under arch-specific directory (RPS fix)

--- lib/pg.rb
+++ lib/pg.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
 
+require 'pathname'
+require 'rbconfig'
+
 # Load the correct version if it's a Windows binary gem
 if RUBY_PLATFORM =~/(mswin|mingw)/i
 	major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
 		raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
 	require "#{major_minor}/pg_ext"
 else
-	require 'pg_ext'
+	archlib = Pathname( __FILE__ ).dirname + Config::CONFIG['arch']
+	requirepath = archlib + 'pg_ext'
+	require( requirepath.to_s )
 end
 
