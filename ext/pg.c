54a4bede535643c44311dc40792ac6ba4aad8765
Michael Granger <ged@FaerieMUD.org>
2010-12-01 18:24:03 -0800

    Finished the merge of default; fixed specs under 1.9.2.
    --HG--
    branch : pg90_notify_payload

--- ext/pg.c
+++ ext/pg.c
@@ -2137,7 +2137,6 @@ pgconn_wait_for_notify(int argc, VALUE *argv, VALUE self)
 	struct timeval timeout;
 	struct timeval *ptimeout = NULL;
 	VALUE timeout_in, relname = Qnil, be_pid = Qnil, extra = Qnil;
-	VALUE block = Qnil;
 	double timeout_sec;
 	fd_set sd_rset;
 
@@ -2171,12 +2170,12 @@ pgconn_wait_for_notify(int argc, VALUE *argv, VALUE self)
 	relname = rb_tainted_str_new2( notification->relname );
 	be_pid = INT2NUM( notification->be_pid );
 #ifdef HAVE_ST_NOTIFY_EXTRA
-		extra = rb_str_new2( notify->extra );
+	extra = rb_str_new2( notification->extra );
 #endif
 	PQfreemem( notification );
 
 	if ( rb_block_given_p() )
-		rb_yield_splat( rb_ary_new3(2, relname, be_pid) );
+		rb_yield_splat( rb_ary_new3(3, relname, be_pid, extra) );
 
 	return relname;
 }
