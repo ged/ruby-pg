be36b4f3586da52d0c7925e491e30b708fa03f7a
Michael Granger <ged@FaerieMUD.org>
2010-12-06 07:44:14 -0800

    Fix require for natively-compiled extension under Windows. (fixes #55)

--- lib/pg.rb
+++ lib/pg.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
 require 'rbconfig'
 
 # Load the correct version if it's a Windows binary gem
 if RUBY_PLATFORM =~/(mswin|mingw)/i
 	major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
 		raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
-	require "#{major_minor}/pg_ext"
+
+	begin
+		require "#{major_minor}/pg_ext"
+	rescue LoadError
+		require 'pg_ext'
+	end
 else
 	require 'pg_ext'
 end
