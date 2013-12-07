9abbc31d8337faab82834192cd0f336da0450a5d
Lars Kanis <lars@greiz-reinsdorf.de>
2013-12-07 19:16:22 +0100

    Add a (quite incomplete) default mapping of OIDs to PG::Result and allow distinct mappings for text and binary formats to Result#map_types! .
    Add documentation to Result#map_types! .

--- lib/pg/result.rb
+++ lib/pg/result.rb
@@ -10,8 +10,50 @@ class PG::Result
 		return enum_for(:each_row).to_a
 	end
 
-	def map_types!(oid_mapping, default_mapping=nil)
-		types = nfields.times.map{|i| oid_mapping[ftype(i)] || default_mapping }
+	DEFAULT_OID_MAP_TEXT = {
+		16 => PG::ColumnMapping::TextBoolean, # BOOLEAN
+		17 => PG::ColumnMapping::TextBytea, # BYTEA
+		20 => PG::ColumnMapping::TextInteger, # INT8
+		21 => PG::ColumnMapping::TextInteger, # INT2
+		23 => PG::ColumnMapping::TextInteger, # INT4
+		700 => PG::ColumnMapping::TextFloat, # FLOAT4
+		701 => PG::ColumnMapping::TextFloat, # FLOAT8
+		705 => PG::ColumnMapping::TextString, # TEXT
+		1082 => proc{|res, tuple, field, string| Time.new(string) }, # DATE
+		1114 => proc{|res, tuple, field, string| Time.new(string) }, # TIMESTAMP WITHOUT TIME ZONE
+		1184 => proc{|res, tuple, field, string| Time.new(string) }, # TIMESTAMP WITH TIME ZONE
+	}
+
+	DEFAULT_OID_MAP_BINARY = {
+		16 => PG::ColumnMapping::BinaryBoolean, # BOOLEAN
+		17 => PG::ColumnMapping::BinaryBytea, # BYTEA
+		20 => PG::ColumnMapping::BinaryInteger, # INT8
+		21 => PG::ColumnMapping::BinaryInteger, # INT2
+		23 => PG::ColumnMapping::BinaryInteger, # INT4
+		700 => PG::ColumnMapping::BinaryFloat, # FLOAT4
+		701 => PG::ColumnMapping::BinaryFloat, # FLOAT8
+		705 => PG::ColumnMapping::BinaryString, # TEXT
+	}
+
+	DEFAULT_OID_MAP = [DEFAULT_OID_MAP_TEXT, DEFAULT_OID_MAP_BINARY]
+
+	# Build and set a ColumnMapping based on the given OID-to-Type Mapping.
+	#
+	# +oid_mapping+: a Hash with OIDs (Integer) as keys and with the corresponding
+	#   type convertion as value part. The type convertion must be suitable
+	#   as a parameter to PG::ColumnMapping.new().
+	#
+	# +default_mapping+: the type conversion that is used, when the given OID is not
+	#   in +oid_mapping+.
+	#
+	# Both +oid_mapping+ and +default_mapping+ can also be wrapped in an Array
+	# with element 0 for mappings of text format and element 1 for binary
+	# format.
+	def map_types!(oid_mapping = DEFAULT_OID_MAP, default_mapping=nil)
+		types = nfields.times.map{|i|
+			(oid_mapping.kind_of?(Array) ? oid_mapping[fformat(i)][ftype(i)] : oid_mapping[ftype(i)]) ||
+			(default_mapping.kind_of?(Array) ? default_mapping[fformat(i)] : default_mapping)
+		}
 		self.column_mapping = PG::ColumnMapping.new( types )
 		self
 	end
--- spec/pg/column_mapping_spec.rb
+++ spec/pg/column_mapping_spec.rb
@@ -38,34 +38,9 @@ describe PG::ColumnMapping do
 	# Examples
 	#
 
-	OID_MAP_TEXT = {
-		16 => PG::ColumnMapping::TextBoolean, # BOOLEAN
-		17 => PG::ColumnMapping::TextBytea, # BYTEA
-		20 => PG::ColumnMapping::TextInteger, # INT8
-		21 => PG::ColumnMapping::TextInteger, # INT2
-		23 => PG::ColumnMapping::TextInteger, # INT4
-		700 => PG::ColumnMapping::TextFloat, # FLOAT4
-		701 => PG::ColumnMapping::TextFloat, # FLOAT8
-		705 => PG::ColumnMapping::TextString, # TEXT
-		1082 => proc{|res, tuple, field, string| Time.new(string) }, # DATE
-		1114 => proc{|res, tuple, field, string| Time.new(string) }, # TIMESTAMP WITHOUT TIME ZONE
-		1184 => proc{|res, tuple, field, string| Time.new(string) }, # TIMESTAMP WITH TIME ZONE
-	}
-
-	OID_MAP_BINARY = {
-		16 => PG::ColumnMapping::BinaryBoolean, # BOOLEAN
-		17 => PG::ColumnMapping::BinaryBytea, # BYTEA
-		20 => PG::ColumnMapping::BinaryInteger, # INT8
-		21 => PG::ColumnMapping::BinaryInteger, # INT2
-		23 => PG::ColumnMapping::BinaryInteger, # INT4
-		700 => PG::ColumnMapping::BinaryFloat, # FLOAT4
-		701 => PG::ColumnMapping::BinaryFloat, # FLOAT8
-		705 => PG::ColumnMapping::BinaryString, # TEXT
-	}
-
 	it "should do OID based type conversions", :ruby_19 do
 		res = @conn.exec( "SELECT 1, 'a', 2.0::FLOAT, TRUE, '2013-06-30'::DATE, generate_series(4,5)" )
-		res.map_types!(OID_MAP_TEXT).values.should == [
+		res.map_types!.values.should == [
 				[ 1, 'a', 2.0, true, Time.new('2013-06-30'), 4 ],
 				[ 1, 'a', 2.0, true, Time.new('2013-06-30'), 5 ],
 		]
@@ -73,7 +48,7 @@ describe PG::ColumnMapping do
 
 	it "should raise an error from default oid type conversion" do
 		res = @conn.exec( "SELECT 'a'::CHAR(1)" )
-		res.map_types!(OID_MAP_TEXT, proc{|res, _, field, _| raise "no converter defined for OID #{res.ftype(field)}" })
+		res.map_types!(PG::Result::DEFAULT_OID_MAP, proc{|res, _, field, _| raise "no converter defined for OID #{res.ftype(field)}" })
 		expect{ res.values }.to raise_error(/no converter defined for OID 1042/)
 	end
 
@@ -105,44 +80,44 @@ describe PG::ColumnMapping do
 	#
 
 	it "should do boolean type conversions" do
-		[[OID_MAP_BINARY, 1], [OID_MAP_TEXT, 0]].each do |map, format|
+		[1, 0].each do |format|
 			res = @conn.exec( "SELECT true::BOOLEAN, false::BOOLEAN, NULL::BOOLEAN", [], format )
-			res.map_types!(map)
+			res.map_types!
 			res.values.should == [[true, false, nil]]
 		end
 	end
 
 	it "should do binary type conversions" do
-		[[OID_MAP_BINARY, 1], [OID_MAP_TEXT, 0]].each do |map, format|
+		[1, 0].each do |format|
 			res = @conn.exec( "SELECT E'\\\\000\\\\377'::BYTEA", [], format )
-			res.map_types!(map)
+			res.map_types!
 			res.values.should == [[["00ff"].pack("H*")]]
 			res.values[0][0].encoding.should == Encoding::ASCII_8BIT if Object.const_defined? :Encoding
 		end
 	end
 
 	it "should do integer type conversions" do
-		[[OID_MAP_BINARY, 1], [OID_MAP_TEXT, 0]].each do |map, format|
+		[1, 0].each do |format|
 			res = @conn.exec( "SELECT -8999::INT2, -899999999::INT4, -8999999999999999999::INT8", [], format )
-			res.map_types!(map)
+			res.map_types!
 			res.values.should == [[-8999, -899999999, -8999999999999999999]]
 		end
 	end
 
 	it "should do string type conversions" do
 		@conn.internal_encoding = 'utf-8' if Object.const_defined? :Encoding
-		[[OID_MAP_BINARY, 1], [OID_MAP_TEXT, 0]].each do |map, format|
+		[1, 0].each do |format|
 			res = @conn.exec( "SELECT 'abcäöü'", [], format )
-			res.map_types!(map)
+			res.map_types!
 			res.values.should == [['abcäöü']]
 			res.values[0][0].encoding.should == Encoding::UTF_8 if Object.const_defined? :Encoding
 		end
 	end
 
 	it "should do float type conversions" do
-		[[OID_MAP_BINARY, 1], [OID_MAP_TEXT, 0]].each do |map, format|
+		[1, 0].each do |format|
 			res = @conn.exec( "SELECT -8.999e3::FLOAT4, 8.999e10::FLOAT4, -8999999999e-99::FLOAT8, NULL::FLOAT4", [], format )
-			res.map_types!(map)
+			res.map_types!
 			res.getvalue(0,0).should be_within(1e-2).of(-8.999e3)
 			res.getvalue(0,1).should be_within(1e5).of(8.999e10)
 			res.getvalue(0,2).should be_within(1e-109).of(-8999999999e-99)
