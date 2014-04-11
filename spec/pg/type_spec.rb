c4b356c0c2cd74d9ae7fec0e5ccfa13355f71517
Lars Kanis <lars@greiz-reinsdorf.de>
2014-04-11 22:52:01 +0200

    Convert and add several new test cases to SimpleType and CompositType.

--- spec/pg/type_spec.rb
+++ spec/pg/type_spec.rb
@@ -16,164 +16,221 @@ require 'spec/lib/helpers'
 require 'pg'
 
 describe PG::Type do
+	let!(:text_int_type) { PG::Type::SimpleType.new encoder: PG::Type::TextEncoder::Integer, decoder: PG::Type::TextDecoder::Integer }
+	let!(:text_float_type) { PG::Type::SimpleType.new encoder: PG::Type::TextEncoder::Float, decoder: PG::Type::TextDecoder::Float }
+	let!(:text_string_type) { PG::Type::SimpleType.new encoder: PG::Type::TextEncoder::String, decoder: PG::Type::TextDecoder::String }
+	let!(:text_timestamp_type) { PG::Type::SimpleType.new encoder: PG::Type::TextEncoder::TimestampWithoutTimeZone, decoder: PG::Type::TextDecoder::TimestampWithoutTimeZone }
+	let!(:binary_int8_type) { PG::Type::SimpleType.new encoder: PG::Type::BinaryEncoder::Int8, decoder: PG::Type::BinaryDecoder::Integer }
 
-	it "should offer decode method with tuple/field" do
-		res = PG::Type::Text::INT8.decode("123", 1, 1)
-		res.should == 123
-	end
-
-	it "should offer decode method without tuple/field" do
-		res = PG::Type::Text::INT8.decode("234")
-		res.should == 234
-	end
-
-	it "should raise when decode method has wrong args" do
-		expect{ PG::Type::Text::INT8.decode() }.to raise_error(ArgumentError)
-		expect{ PG::Type::Text::INT8.decode("123", 2, 3, 4) }.to raise_error(ArgumentError)
-		expect{ PG::Type::Text::INT8.decode(2, 3, 4) }.to raise_error(TypeError)
-	end
-
-	it "should offer encode method for text type" do
-		res = PG::Type::Text::INT8.encode(123)
-		res.should == "123"
-	end
-
-	it "should offer encode method for binary type" do
-		res = PG::Type::Binary::INT4.encode(123)
-		res.should == [123].pack("N")
-	end
-
-	it "should encode integers of different length to text format" do
-		30.times do |zeros|
-			PG::Type::Text::INT8.encode(10 ** zeros).should == "1" + "0"*zeros
-		end
-	end
-
-	describe 'PG::Type::Text::TEXTARRAY' do
-		let!(:text_array_type) { PG::Type::Text::TEXTARRAY }
-		let!(:int8_array_type) { PG::Type::Text::INT8ARRAY }
-		let!(:float8_array_type) { PG::Type::Text::FLOAT8ARRAY }
-		let!(:timestamp_array_type) { PG::Type::Text::TIMESTAMPARRAY }
-
-		#
-		# Array parser specs are thankfully borrowed from here:
-		# https://github.com/dockyard/pg_array_parser
-		#
+	describe PG::Type::SimpleType do
 		describe '#decode' do
-			context 'one dimensional arrays' do
-				context 'empty' do
-					it 'returns an empty array' do
-						text_array_type.decode(%[{}]).should eq []
-					end
-				end
-
-				context 'no strings' do
-					it 'returns an array of strings' do
-						text_array_type.decode(%[{1,2,3}]).should eq ['1','2','3']
-					end
-				end
-
-				context 'NULL values' do
-					it 'returns an array of strings, with nils replacing NULL characters' do
-						text_array_type.decode(%[{1,NULL,NULL}]).should eq ['1',nil,nil]
-					end
-				end
-
-				context 'quoted NULL' do
-					it 'returns an array with the word NULL' do
-						text_array_type.decode(%[{1,"NULL",3}]).should eq ['1','NULL','3']
-					end
-				end
-
-				context 'strings' do
-					it 'returns an array of strings when containing commas in a quoted string' do
-						text_array_type.decode(%[{1,"2,3",4}]).should eq ['1','2,3','4']
-					end
-
-					it 'returns an array of strings when containing an escaped quote' do
-						text_array_type.decode(%[{1,"2\\",3",4}]).should eq ['1','2",3','4']
-					end
-
-					it 'returns an array of strings when containing an escaped backslash' do
-						text_array_type.decode(%[{1,"2\\\\",3,4}]).should eq ['1','2\\','3','4']
-						text_array_type.decode(%[{1,"2\\\\\\",3",4}]).should eq ['1','2\\",3','4']
-					end
-
-					it 'returns an array containing empty strings' do
-						text_array_type.decode(%[{1,"",3,""}]).should eq ['1', '', '3', '']
-					end
-
-					it 'returns an array containing unicode strings' do
-						text_array_type.decode(%[{"Paragraph 399(b)(i) – “valid leave” – meaning"}]).should eq(['Paragraph 399(b)(i) – “valid leave” – meaning'])
-					end
-				end
+			it "should offer decode method with tuple/field" do
+				res = text_int_type.decode("123", 1, 1)
+				res.should == 123
 			end
 
-			context 'two dimensional arrays' do
-				context 'empty' do
-					it 'returns an empty array' do
-						text_array_type.decode(%[{{}}]).should eq [[]]
-						text_array_type.decode(%[{{},{}}]).should eq [[],[]]
-					end
-				end
-				context 'no strings' do
-					it 'returns an array of strings with a sub array' do
-						text_array_type.decode(%[{1,{2,3},4}]).should eq ['1',['2','3'],'4']
-					end
-				end
-				context 'strings' do
-					it 'returns an array of strings with a sub array' do
-						text_array_type.decode(%[{1,{"2,3"},4}]).should eq ['1',['2,3'],'4']
-					end
-					it 'returns an array of strings with a sub array and a quoted }' do
-						text_array_type.decode(%[{1,{"2,}3",NULL},4}]).should eq ['1',['2,}3',nil],'4']
-					end
-					it 'returns an array of strings with a sub array and a quoted {' do
-						text_array_type.decode(%[{1,{"2,{3"},4}]).should eq ['1',['2,{3'],'4']
-					end
-					it 'returns an array of strings with a sub array and a quoted { and escaped quote' do
-						text_array_type.decode(%[{1,{"2\\",{3"},4}]).should eq ['1',['2",{3'],'4']
-					end
-					it 'returns an array of strings with a sub array with empty strings' do
-						text_array_type.decode(%[{1,{""},4,{""}}]).should eq ['1',[''],'4',['']]
-					end
-				end
-				context 'timestamps' do
-					it 'decodes an array of timestamps with sub arrays' do
-						timestamp_array_type.decode('{2014-12-31 00:00:00,{NULL,2016-01-02 23:23:59.0000000}}').
-							should eq [Time.new(2014,12,31),[nil, Time.new(2016,01,02, 23, 23, 59)]]
-					end
-				end
+			it "should offer decode method without tuple/field" do
+				res = text_int_type.decode("234")
+				res.should == 234
 			end
-			context 'three dimensional arrays' do
-				context 'empty' do
-					it 'returns an empty array' do
-						text_array_type.decode(%[{{{}}}]).should eq [[[]]]
-						text_array_type.decode(%[{{{},{}},{{},{}}}]).should eq [[[],[]],[[],[]]]
-					end
-				end
-				it 'returns an array of strings with sub arrays' do
-					text_array_type.decode(%[{1,{2,{3,4}},{NULL,6},7}]).should eq ['1',['2',['3','4']],[nil,'6'],'7']
-				end
+
+			it "should decode with ruby decoder" do
+				ruby_type = PG::Type::SimpleType.new decoder: proc{|v| v.to_i+1 }
+				ruby_type.decode("3").should eq 4
+			end
+
+			it "should raise when decode method is called with wrong args" do
+				expect{ text_int_type.decode() }.to raise_error(ArgumentError)
+				expect{ text_int_type.decode("123", 2, 3, 4) }.to raise_error(ArgumentError)
+				expect{ text_int_type.decode(2, 3, 4) }.to raise_error(TypeError)
+				ruby_type = PG::Type::SimpleType.new decoder: proc{|v| v.to_i+1 }
+				expect{ ruby_type.decode(2, 3, 4) }.to raise_error(TypeError)
 			end
 		end
 
 		describe '#encode' do
-			context 'three dimensional arrays' do
-				it 'encodes an array of strings and numbers with sub arrays' do
-					text_array_type.encode(['1',['2',['3','4']],[nil,6],7.8]).should eq %[{"1",{"2",{"3","4"}},{NULL,"6"},"7.8"}]
-				end
-				it 'encodes an array of int8 with sub arrays' do
-					int8_array_type.encode([1,[2,[3,4]],[nil,6],7]).should eq %[{1,{2,{3,4}},{NULL,6},7}]
-				end
-				it 'encodes an array of float8 with sub arrays' do
-					float8_array_type.encode([1000.11,[-0.00221,[3.31,-441]],[nil,6.61],-7.71]).should match Regexp.new(%[^{1.0001*E+03,{-2.2*E-03,{3.3*E+00,-4.4*E+02}},{NULL,6.6*E+00},-7.7*E+00}$].gsub(/([\.\+\{\}\,])/, "\\\\\\1").gsub(/\*/, "\\d*"))
+			it "should offer encode method for text type" do
+				res = text_int_type.encode(123)
+				res.should == "123"
+			end
+
+			it "should offer encode method for binary type" do
+				res = binary_int8_type.encode(123)
+				res.should == [123].pack("q>")
+			end
+
+			it "should encode integers of different length to text format" do
+				30.times do |zeros|
+					text_int_type.encode(10 ** zeros).should == "1" + "0"*zeros
 				end
 			end
-			context 'two dimensional arrays' do
-				it 'encodes an array of timestamps with sub arrays' do
-					timestamp_array_type.encode([Time.new(2014,12,31),[nil, Time.new(2016,01,02, 23, 23, 59.99)]]).
-							should eq %[{2014-12-31 00:00:00.000000000,{NULL,2016-01-02 23:23:59.990000000}}]
+
+			it "should encode with ruby encoder" do
+				ruby_type = PG::Type::SimpleType.new encoder: proc{|v| (v+1).to_s }
+				ruby_type.encode(3).should eq "4"
+			end
+
+			it "should raise when ruby encoder returns non string values" do
+				ruby_type = PG::Type::SimpleType.new encoder: proc{|v| v+1 }
+				expect{ ruby_type.encode(3) }.to raise_error(TypeError)
+			end
+		end
+	end
+
+	describe PG::Type::CompositeType do
+		describe "Array types" do
+			let!(:text_string_array_type) { PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, decoder: PG::Type::TextDecoder::Array, elements_type: text_string_type }
+			let!(:text_int_array_type) { PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, decoder: PG::Type::TextDecoder::Array, elements_type: text_int_type, needs_quotation: false }
+			let!(:text_float_array_type) { PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, decoder: PG::Type::TextDecoder::Array, elements_type: text_float_type, needs_quotation: false }
+			let!(:text_timestamp_array_type) { PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, decoder: PG::Type::TextDecoder::Array, elements_type: text_timestamp_type, needs_quotation: false }
+
+			#
+			# Array parser specs are thankfully borrowed from here:
+			# https://github.com/dockyard/pg_array_parser
+			#
+			describe '#decode' do
+				context 'one dimensional arrays' do
+					context 'empty' do
+						it 'returns an empty array' do
+							text_string_array_type.decode(%[{}]).should eq []
+						end
+					end
+
+					context 'no strings' do
+						it 'returns an array of strings' do
+							text_string_array_type.decode(%[{1,2,3}]).should eq ['1','2','3']
+						end
+					end
+
+					context 'NULL values' do
+						it 'returns an array of strings, with nils replacing NULL characters' do
+							text_string_array_type.decode(%[{1,NULL,NULL}]).should eq ['1',nil,nil]
+						end
+					end
+
+					context 'quoted NULL' do
+						it 'returns an array with the word NULL' do
+							text_string_array_type.decode(%[{1,"NULL",3}]).should eq ['1','NULL','3']
+						end
+					end
+
+					context 'strings' do
+						it 'returns an array of strings when containing commas in a quoted string' do
+							text_string_array_type.decode(%[{1,"2,3",4}]).should eq ['1','2,3','4']
+						end
+
+						it 'returns an array of strings when containing an escaped quote' do
+							text_string_array_type.decode(%[{1,"2\\",3",4}]).should eq ['1','2",3','4']
+						end
+
+						it 'returns an array of strings when containing an escaped backslash' do
+							text_string_array_type.decode(%[{1,"2\\\\",3,4}]).should eq ['1','2\\','3','4']
+							text_string_array_type.decode(%[{1,"2\\\\\\",3",4}]).should eq ['1','2\\",3','4']
+						end
+
+						it 'returns an array containing empty strings' do
+							text_string_array_type.decode(%[{1,"",3,""}]).should eq ['1', '', '3', '']
+						end
+
+						it 'returns an array containing unicode strings' do
+							text_string_array_type.decode(%[{"Paragraph 399(b)(i) – “valid leave” – meaning"}]).should eq(['Paragraph 399(b)(i) – “valid leave” – meaning'])
+						end
+					end
+				end
+
+				context 'two dimensional arrays' do
+					context 'empty' do
+						it 'returns an empty array' do
+							text_string_array_type.decode(%[{{}}]).should eq [[]]
+							text_string_array_type.decode(%[{{},{}}]).should eq [[],[]]
+						end
+					end
+					context 'no strings' do
+						it 'returns an array of strings with a sub array' do
+							text_string_array_type.decode(%[{1,{2,3},4}]).should eq ['1',['2','3'],'4']
+						end
+					end
+					context 'strings' do
+						it 'returns an array of strings with a sub array' do
+							text_string_array_type.decode(%[{1,{"2,3"},4}]).should eq ['1',['2,3'],'4']
+						end
+						it 'returns an array of strings with a sub array and a quoted }' do
+							text_string_array_type.decode(%[{1,{"2,}3",NULL},4}]).should eq ['1',['2,}3',nil],'4']
+						end
+						it 'returns an array of strings with a sub array and a quoted {' do
+							text_string_array_type.decode(%[{1,{"2,{3"},4}]).should eq ['1',['2,{3'],'4']
+						end
+						it 'returns an array of strings with a sub array and a quoted { and escaped quote' do
+							text_string_array_type.decode(%[{1,{"2\\",{3"},4}]).should eq ['1',['2",{3'],'4']
+						end
+						it 'returns an array of strings with a sub array with empty strings' do
+							text_string_array_type.decode(%[{1,{""},4,{""}}]).should eq ['1',[''],'4',['']]
+						end
+					end
+					context 'timestamps' do
+						it 'decodes an array of timestamps with sub arrays' do
+							text_timestamp_array_type.decode('{2014-12-31 00:00:00,{NULL,2016-01-02 23:23:59.0000000}}').
+								should eq [Time.new(2014,12,31),[nil, Time.new(2016,01,02, 23, 23, 59)]]
+						end
+					end
+				end
+				context 'three dimensional arrays' do
+					context 'empty' do
+						it 'returns an empty array' do
+							text_string_array_type.decode(%[{{{}}}]).should eq [[[]]]
+							text_string_array_type.decode(%[{{{},{}},{{},{}}}]).should eq [[[],[]],[[],[]]]
+						end
+					end
+					it 'returns an array of strings with sub arrays' do
+						text_string_array_type.decode(%[{1,{2,{3,4}},{NULL,6},7}]).should eq ['1',['2',['3','4']],[nil,'6'],'7']
+					end
+				end
+
+				it 'should decode array of types with decoder in ruby space' do
+					ruby_type = PG::Type::SimpleType.new decoder: proc{|v| v.to_i+1 }
+					array_type = PG::Type::CompositeType.new decoder: PG::Type::TextDecoder::Array, elements_type: ruby_type
+					array_type.decode(%[{3,4}]).should eq [4,5]
+				end
+			end
+
+			describe '#encode' do
+				context 'three dimensional arrays' do
+					it 'encodes an array of strings and numbers with sub arrays' do
+						text_string_array_type.encode(['1',['2',['3','4']],[nil,6],7.8]).should eq %[{"1",{"2",{"3","4"}},{NULL,"6"},"7.8"}]
+					end
+					it 'encodes an array of int8 with sub arrays' do
+						text_int_array_type.encode([1,[2,[3,4]],[nil,6],7]).should eq %[{1,{2,{3,4}},{NULL,6},7}]
+					end
+					it 'encodes an array of float8 with sub arrays' do
+						text_float_array_type.encode([1000.11,[-0.00221,[3.31,-441]],[nil,6.61],-7.71]).should match Regexp.new(%[^{1.0001*E+03,{-2.2*E-03,{3.3*E+00,-4.4*E+02}},{NULL,6.6*E+00},-7.7*E+00}$].gsub(/([\.\+\{\}\,])/, "\\\\\\1").gsub(/\*/, "\\d*"))
+					end
+				end
+				context 'two dimensional arrays' do
+					it 'encodes an array of timestamps with sub arrays' do
+						text_timestamp_array_type.encode([Time.new(2014,12,31),[nil, Time.new(2016,01,02, 23, 23, 59.99)]]).
+								should eq %[{2014-12-31 00:00:00.000000000,{NULL,2016-01-02 23:23:59.990000000}}]
+					end
+				end
+
+
+				context 'array of types with encoder in ruby space' do
+					it 'encodes with quotation' do
+						ruby_type = PG::Type::SimpleType.new encoder: proc{|v| (v+1).to_s }
+						array_type = PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, elements_type: ruby_type, needs_quotation: true
+						array_type.encode([3,4]).should eq %[{"4","5"}]
+					end
+
+					it 'encodes without quotation' do
+						ruby_type = PG::Type::SimpleType.new encoder: proc{|v| (v+1).to_s }
+						array_type = PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, elements_type: ruby_type, needs_quotation: false
+						array_type.encode([3,4]).should eq %[{4,5}]
+					end
+
+					it "should raise when ruby encoder returns non string values" do
+						ruby_type = PG::Type::SimpleType.new encoder: proc{|v| v+1 }
+						array_type = PG::Type::CompositeType.new encoder: PG::Type::TextEncoder::Array, elements_type: ruby_type, needs_quotation: false
+						expect{ array_type.encode([3,4]) }.to raise_error(TypeError)
+					end
 				end
 			end
 		end
