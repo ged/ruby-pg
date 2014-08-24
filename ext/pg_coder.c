2767b9f9802de3ed0fb5599d277986862578805b
Lars Kanis <lars@greiz-reinsdorf.de>
2014-08-24 18:33:41 +0200

    Remove unused accessors.

--- ext/pg_coder.c
+++ ext/pg_coder.c
@@ -26,6 +26,4 @@ init_pg_coder()
 {
 	rb_cPG_Coder = rb_define_class_under( rb_mPG, "Coder", rb_cObject );
 	rb_define_attr( rb_cPG_Coder, "name", 1, 0 );
-	rb_define_attr( rb_cPG_Coder, "format", 1, 0 );
-	rb_define_attr( rb_cPG_Coder, "direction", 1, 0 );
 }
