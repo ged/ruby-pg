c6ade8b6b591d1288910f6bb87b157859dd3d7ef
Lars Kanis <lars@greiz-reinsdorf.de>
2014-09-21 21:32:59 +0200

    Update the introduction for type casting in Pg. Luckily it became easier.

--- lib/pg/type_map_by_column.rb
+++ lib/pg/type_map_by_column.rb
@@ -2,7 +2,13 @@
 
 require 'pg' unless defined?( PG )
 
-
+# A TypeMapByColumn combines multiple types together, suitable to convert the
+# input or result values of a given query. TypeMapByColumns are in particular
+# useful in conjunction with prepared statements, since they can be cached
+# alongside with the statement handle.
+#
+# This type map strategy is also used internally by TypeMapByOid, when the
+# number of rows of a result set exceeds a given limit.
 class PG::TypeMapByColumn
 	def oids
 		coders.map{|c| c.oid if c }
