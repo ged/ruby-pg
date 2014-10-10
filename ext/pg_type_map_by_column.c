7d2fb9aaebbfb520bee988365044d0e9c79dcff7
Lars Kanis <lars@greiz-reinsdorf.de>
2014-10-10 20:59:10 +0200

    Use C99 struct initializer syntax.

--- ext/pg_type_map_by_column.c
+++ ext/pg_type_map_by_column.c
@@ -116,12 +116,12 @@ pg_tmbc_typecast_copy_get( t_typemap *p_typemap, VALUE field_str, int fieldno, i
 }
 
 const t_typemap pg_tmbc_default_typemap = {
-	fit_to_result: pg_tmbc_fit_to_result,
-	fit_to_query: pg_tmbc_fit_to_query,
-	fit_to_copy_get: pg_tmbc_fit_to_copy_get,
-	typecast_result_value: pg_tmbc_result_value,
-	typecast_query_param: pg_tmbc_typecast_query_param,
-	typecast_copy_get: pg_tmbc_typecast_copy_get
+	.fit_to_result = pg_tmbc_fit_to_result,
+	.fit_to_query = pg_tmbc_fit_to_query,
+	.fit_to_copy_get = pg_tmbc_fit_to_copy_get,
+	.typecast_result_value = pg_tmbc_result_value,
+	.typecast_query_param = pg_tmbc_typecast_query_param,
+	.typecast_copy_get = pg_tmbc_typecast_copy_get
 };
 
 static void
