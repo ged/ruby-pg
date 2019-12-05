6b34d551493fa3116d3855582c8d37f8f9c3012f
Lars Kanis <lars@greiz-reinsdorf.de>
2019-12-05 17:04:50 +0100

    No need to make a constant lookup

--- lib/pg/text_decoder.rb
+++ lib/pg/text_decoder.rb
@@ -7,10 +7,8 @@ require 'json'
 module PG
 	module TextDecoder
 		class Date < SimpleDecoder
-			ISO_DATE = /\A(\d{4})-(\d\d)-(\d\d)\z/
-
 			def decode(string, tuple=nil, field=nil)
-				if string =~ ISO_DATE
+				if string =~ /\A(\d{4})-(\d\d)-(\d\d)\z/
 					::Date.new $1.to_i, $2.to_i, $3.to_i
 				else
 					string
