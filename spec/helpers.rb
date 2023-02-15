0a9dc3856a3df54d3e537e198f443a093305413c
Lars Kanis <lars@greiz-reinsdorf.de>
2023-02-15 20:59:16 +0100

    Exclude scheduler tests on ruby-3.0 on Windows
    On Windows ruby-3.1 is needed.

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -688,7 +688,7 @@ RSpec.configure do |config|
 	config.filter_run_excluding( :postgresql_12 ) if PG.library_version < 120000
 	config.filter_run_excluding( :postgresql_14 ) if PG.library_version < 140000
 	config.filter_run_excluding( :unix_socket ) if RUBY_PLATFORM=~/mingw|mswin/i
-	config.filter_run_excluding( :scheduler ) if RUBY_VERSION < "3.0" || !Fiber.respond_to?(:scheduler)
+	config.filter_run_excluding( :scheduler ) if RUBY_VERSION < "3.0" || (RUBY_PLATFORM =~ /mingw|mswin/ && RUBY_VERSION < "3.1") || !Fiber.respond_to?(:scheduler)
 	config.filter_run_excluding( :scheduler_address_resolve ) if RUBY_VERSION < "3.1"
 	config.filter_run_excluding( :ipv6 ) if Addrinfo.getaddrinfo("localhost", nil, nil, :STREAM).size < 2
 
