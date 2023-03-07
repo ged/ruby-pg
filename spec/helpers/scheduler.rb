e84ec6ed0e6b8a98c75c770d3d284e54b2307a5a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-03-07 15:34:26 +1300

    Add `io_select` hook.

--- spec/helpers/scheduler.rb
+++ spec/helpers/scheduler.rb
@@ -196,6 +196,13 @@ class Scheduler
 		@waiting.delete(Fiber.current) if duration
 	end
 
+	def io_select(...)
+		# Emulate the operation using a non-blocking thread:
+		Thread.new do
+			IO.select(...)
+		end.value
+	end
+
 	# Used for Kernel#sleep and Thread::Mutex#sleep
 	def kernel_sleep(duration = nil)
 		# $stderr.puts [__method__, duration, Fiber.current].inspect
