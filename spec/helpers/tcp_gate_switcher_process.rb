069eb79938e063418ae871047caf1e8ab7258362
Lars Kanis <lars@greiz-reinsdorf.de>
2026-09-17 20:05:55 +0200

    Fix TcpGateSwitcherProcess on Appveyor on Windows
    It fails for some reason when STDIN is used to send the ruby code, although it works on other Windows systems:
    ```
    3) PG::Connection connects without port and then retrieves the default port
         Failure/Error: server_uri = io.gets.strip
         NoMethodError:
           undefined method 'strip' for nil
          ./spec/helpers/tcp_gate_switcher_process.rb:38:in 'Helpers::TcpGateSwitcherProcess#initialize'
          ./spec/pg/connection_spec.rb:902:in 'block (2 levels) in <top (required)>'
          ./spec/helpers.rb:64:in 'block in PG::TestingHelpers.included'
    ```

--- spec/helpers/tcp_gate_switcher_process.rb
+++ spec/helpers/tcp_gate_switcher_process.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'drb/drb'
+require 'rbconfig'
 
 # This is a wrapper of TcpGateSwitcher running in a separate process to avoid the need of threads.
 # It can therefore be used in conjunction with blocking GVL locking functions.
@@ -8,37 +9,11 @@ require 'drb/drb'
 module Helpers
 class TcpGateSwitcherProcess
 	def initialize(**kwargs)
-		file = File.expand_path("tcp_gate_switcher", __dir__)
-		rbtext = <<~RBTEXT
-			require #{file.inspect}
-			require "drb/drb"
-
-			switcher = Helpers::TcpGateSwitcher.allocate
-			def switcher.finish
-				super
-				DRb.stop_service
-			end
-			def switcher.init
-				initialize(**#{kwargs.inspect})
-				self
-			end
-			DRb.start_service('druby://localhost:0', switcher)
-			puts DRb.uri
-			# Redirect STDOUT to STDERR, so that p prints to STDERR
-			STDOUT.reopen(STDERR)
-
-			# Wait for the drb server thread to finish before exiting.
-			DRb.thread.join
-			# puts "TcpGateSwitcherProcess finished"
-		RBTEXT
-
-		io = IO.popen("ruby", "w+")
-		io.write rbtext
-		io.close_write
-		server_uri = io.gets.strip
+		@io = IO.popen([RbConfig::CONFIG['ruby_install_name'], __FILE__])
+		server_uri = @io.gets.strip
 		@server = DRbObject.new_with_uri(server_uri)
 		# Call initialize through DRb, so that Exceptions are passed to caller
-		@server.init
+		@server.init(kwargs)
 	rescue
 		@server&.finish
 		raise
@@ -50,4 +25,26 @@ class TcpGateSwitcherProcess
 		end
 	end
 end
+end
+
+if $0 == __FILE__
+	require_relative "tcp_gate_switcher"
+
+	switcher = Helpers::TcpGateSwitcher.allocate
+	def switcher.finish
+		super
+		DRb.stop_service
+	end
+	def switcher.init(kwargs)
+		initialize(**kwargs)
+		self
+	end
+	DRb.start_service('druby://localhost:0', switcher)
+	puts DRb.uri
+	# Redirect STDOUT to STDERR, so that p prints to STDERR
+	STDOUT.reopen(STDERR)
+
+	# Wait for the drb server thread to finish before exiting.
+	DRb.thread.join
+	# puts "TcpGateSwitcherProcess finished"
 end
